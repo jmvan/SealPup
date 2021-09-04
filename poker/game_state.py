a0a737e950258dd158da479476b0dc07c0f6841d
James Van <birdybands@Jamess-MacBook-Pro.local>
2021-09-04 13:03:16 -0700

    Refactoring game state

--- poker/game_state.py
+++ poker/game_state.py
@@ -4,10 +4,9 @@ from game import Game
 
 class State(Enum):
     NO_GAME = 1
-    GAME_INIT = 2
     WAIT_FOR_PLAYERS = 2
     ROTATE_BLIND = 3
-    BETTING = 4
+    BETTING_FLOP = 4
     DEALING_FLOP = 5
     DEALING_TURN = 6
     DEALING_RIVER = 7
@@ -39,25 +38,31 @@ class GameState():
         elif command == "!help":
             await message.channel.send("help command")
         elif command == "!exit":
-            game = None
+            self.game = None
             await message.channel.send("The game has ended, thanks for playing!")
+        else:
+            await message.channel.send("Command not recognized, please type `!help` to see the list of commands.")
 
     def process_new_game(self, message: discord.Message, commands: str):
         if self.game_state == State.NO_GAME:
             self.game = Game(commands)
-            self.game_state = State.GAME_INIT
+            self.game_state = State.WAITING_FOR_
         await message.channel.send("The Poker game is currently looking for players\n" +
                                    "Type `!join` to join the game\n" +
                                    "Type '!deal` to start the game")
 
-    def process_new_player(self, ):
+    def process_new_player(self, message: discord.Message):
+        if self.game_state == State.WAIT_FOR_PLAYERS:
+
+
+        else:
+
         self.game.add_player()
 
     def process_dealing(self):
 
-    def process_player_call(self):
 
-    def process_player_raise(self):
+    def process_player_call(self):
 
     def process_player_raise(self):
 
