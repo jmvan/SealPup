27c4d140f976753459aed5b4ad2b86edb584b2d8
James Van <james.maivan@gmail.com>
2021-09-07 18:56:43 -0700

    Changing FSM implementation

--- poker/poker.py
+++ poker/poker.py
@@ -37,80 +37,34 @@ class Poker:
         elif first_command == "?deal":
             self.game.game_state = GameState.BETTING_PRE_FLOP
             self.game.deal_hole_cards()
+            # TODO: send message on who is currently big blind and small blind and whose turn it is
 
     def process_betting_pre_flop(self, first_command: str, message: discord.Message) -> None:
-        if
-        if first_command == "?check":
+        if not self.game.check_current_player(message.author):
 
-
-    def process_command(self, message: discord.Message) -> None:
-        first_command = message.content.split()[0]
-        if first_command == "?poker":
-            self.process_new_game(message)
-        elif first_command == "?join":
-            self.process_new_player(message=message)
-        elif first_command == "?deal":
-            self.process_dealing(message=message)
+            await message.channel.send("")
         elif first_command == "?check":
-            self.process_player_check()
+            self.game.player_check()
         elif first_command == "?call":
-            self.process_player_call()
+            self.game.player_call()
         elif first_command == "?raise":
-            self.process_player_raise()
+            self.game.player_raise()
         elif first_command == "?fold":
-            self.process_player_fold()
+            self.game.player_fold()
         elif first_command == "?allin":
-            self.process_player_all_in()
+            self.game.player.
         elif first_command == "?help":
-            await message.channel.send("help command")
         elif first_command == "?exit":
-            self.game = None
-            self.game.game_state = GameState.NO_GAME
-            await message.channel.send("The game has ended, thanks for playing!")
         else:
-            await message.channel.send("Command not recognized, please type `?help` to see the list of commands.")
 
-    def process_new_player(self, message: discord.Message):
-        if self.game.game_state == GameState.NO_GAME:
-            await message.channel.send("The poker game has not started yet\n" +
-                                       "Type `?poker` to start looking for players")
-        elif self.game.game_state == GameState.WAIT_FOR_PLAYERS:
-            self.game.add_player(message.author)
-            await message.channel.send('{} has joined the poker game.'.format(message.author.name))
-        else:
-            await message.channel.send('{} cannot join the poker game.'.format(message.author.name))
-            # TODO: add the player, but wait for next round
-
-    def process_dealing(self, message: discord.Message):
-        if self.game.game_state == GameState.NO_GAME:
-            await message.channel.send("The poker game has not started yet\n" +
-                                       "Type `?poker` to start looking for players")
-        elif self.game.game_state == GameState.WAIT_FOR_PLAYERS:
-            self.game.game_state = GameState.BETTING_PREFLOP
-            self.game.deal_hole_cards() # send two cards to players
-        else:
-            await message.channel.send("The poker game has already started and the cards have been dealt.")
-
-    def process_preflop(self):
-        if self.game.game_state == GameState.NO_GAME:
-            await message.channel.send("The poker game has not started yet\n" +
-                                       "Type `?poker` to start looking for players")
-        elif self.game.game_state == GameState.WAIT_FOR_PLAYERS:
 
 
     def process_player_check(self):
-        if self.game.game_state == GameState.BETTING_PREFLOP:
-        elif self.game.game_state == GameState.BETTING
-
-
     def process_player_call(self):
-
     def process_player_raise(self):
-
-
     def process_player_fold(self):
-
     def process_player_all_in(self):
-        # check if the player is still in
+
+
 
 
